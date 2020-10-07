8ac4033f9cc41fdad325a9eb1c11625000268f12
Carl J <carlj1881@yahoo.com>
2020-10-07 05:29:09 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,30 @@
-class Meal
+require "pry"
 
+
+class Meal
+  
+  attr_accessor :waiter, :customer, :total, :tip
+  
+  @@all = []
+  
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+    
+  def self.all
+    @@all
+  end
+  
+  #binding.pry
+  def meals
+    return @@all.meals
+  end
+  
 end
+
+# #meals
+#       returns an Array of Meal instances associated with this customer (FAILED - 2)
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,34 @@
+require "pry"
+
+
 class Waiter
 
+  attr_accessor :name, :experience
+
+  @@all = []
+
+  def initialize(name, experience)
+    @name = name
+    @experience = experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip)
+   Meal.new(self, customer, total, tip)
+  end
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self
+    end
+  end
+  
 end
+
+
+# #waiters
+#       returns an Array of Waiter instances associated with this customer's meals
