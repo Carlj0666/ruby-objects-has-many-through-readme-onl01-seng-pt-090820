e6e60f2f6ac6dd9b94ebc2f6c91d02ffe8ea37d0
Carl J <carlj1881@yahoo.com>
2020-10-07 06:42:38 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -31,7 +31,7 @@ class Waiter
     biggest_tip = 0
     biggest_tipper = "Uncle Money Bags"
     
-    Meal.all.select do |meal, customer|
+    Meal.all.select do |meal|
       # binding.pry
       if biggest_tip < meal.tip
         meal.tip = biggest_tip
