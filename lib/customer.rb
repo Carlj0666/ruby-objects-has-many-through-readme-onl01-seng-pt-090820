0fe220d9192eca74c287ef1ba94a26eba92408bd
Carl J <carlj1881@yahoo.com>
2020-10-07 06:04:01 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -29,6 +29,7 @@ class Customer
   def waiters
     my_waiters = []
     Meal.all.select do |meal|
+      if meal.customer == self
       my_waiter = meal.waiter
       my_waiters << my_waiter
       
@@ -38,3 +39,5 @@ class Customer
   
 end
 
+expected: @experience=1>,#<Waiter:0x0000000002f181a8 @name="Joe", @experience=10>, #<Waiter:0x0000000002f18108 @name="Andrew", @experience=3>]
+     got: @experience=1>,#<Waiter:0x0000000002f181a8 @name="Joe...f181f8 @name="Terrance", @experience=1>, #<Waiter:0x0000000002f18158 @name="Esmery", @experience=2>]
